6bcd505689ae94f84d7bcd9e74fbd1233b1b579d
Matheus de Menezes <mat112011@hotmail.com>
2026-09-04 07:41:49 -0300

    feat: implementa solucao do exercicio 01

---
+++ 01_modelagem_tuplas.py
@@ -0,0 +1,4 @@
+registro_cliente = (101, "Ana Silva", "ana@email.com", 3500.50, True)
+id_cliente, nome, email, salario, status = registro_cliente
+
+print(f"REGISTRO CARREGADO: ID: {id_cliente} | Nome: {nome} | Salário: R$ {salario} | Ativo: {status}")
